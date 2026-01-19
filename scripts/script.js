f7dafc35df1294b4214cd1fcc12a26eb82f5f0da
swagswagdabdab <112865075+swagswagdabdab@users.noreply.github.com>
2026-01-19 04:09:19 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
-// JavaScript Document
-console.log("hi");
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY && currentScrollY > 80) {
+    // scroll naar beneden → verberg nav
+    header.classList.add("poef");
+  } else {
+    // scroll naar boven → toon nav
+    header.classList.remove("poef");
+  }
+
+  lastScrollY = currentScrollY;
+});
+
